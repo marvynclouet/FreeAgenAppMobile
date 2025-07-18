e89eb3dd197ea8ba6296684fc3d39355f00ae51b
marvynclouet <clouetmarvyn@gmail.com>
2025-07-19 01:38:55 +0200

    Fix database config and use environment variables

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  host: 'hopper.proxy.rlwy.net',
-  user: 'root',
-  password: 'WkdwbGCWQjoQhjNdeGEumAVztCSRXvZn',
-  database: 'railway',
-  port: 24981
+  host: process.env.DB_HOST || 'hopper.proxy.rlwy.net',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'WkdwbGCWQjoQhjNdeGEumAVztCSRXvZn',
+  database: process.env.DB_NAME || 'railway',
+  port: process.env.DB_PORT || 24981
 }; 
